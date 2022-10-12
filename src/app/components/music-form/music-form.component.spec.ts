3b32f922f9149b762d2b917383de410cb2b8e3d4
PROPIO-GITHUB <victor.ortiz.w@gmail.com>
2022-10-12 16:54:33 -0300

    feat: (RED) Sould card

--- src/app/components/music-form/music-form.component.spec.ts
+++ src/app/components/music-form/music-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ISound } from 'src/app/shared/interfaces/sound.interfaces';
 
 import { MusicFormComponent } from './music-form.component';
 
@@ -18,6 +19,11 @@ describe('MusicFormComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    component.sounds = []
+    fixture.detectChanges();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -25,8 +31,26 @@ describe('MusicFormComponent', () => {
   describe('Sould card', () => {
     it('sould render card', () => {
 
+      const soundsMock: ISound[] = [
+        {
+          category: 'Amor',
+          description: 'El amor de Dios es maravillosos',
+          duration: 30000,
+          id: 1
+        }
+      ]
+
+      component.sounds = soundsMock;
+      fixture.detectChanges();
+
       const debugElement = fixture.debugElement.query(By.css('div#card-sound'));
       expect(debugElement).toBeTruthy();
+    })
+
+    it('sould not render with sounds is < 0', () => {
+      const debugElement = fixture.debugElement.query(By.css('div#card-sound'));
+      expect(debugElement).toBeFalsy();
+
     })
 
   })
